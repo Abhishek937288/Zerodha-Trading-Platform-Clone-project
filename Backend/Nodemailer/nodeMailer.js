2e80aeae3a4545fe8cad69cece9fd6405d3759b6
Abhishek <mankumbareabhi@gmail.com>
2025-11-07 22:55:18 +0530

    fix : trying to fix the nodemailer error

--- Backend/Nodemailer/nodeMailer.js
+++ Backend/Nodemailer/nodeMailer.js
@@ -6,14 +6,28 @@ const nodemailerPassword = env("PASSWORD");
 
 import nodemailer from "nodemailer";
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: nodemailerEmail,
+//     pass: nodemailerPassword,
+//   },
+//   logger: true,
+//   debug: true,
+// });
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: isProduction ? 465 : 587, // 465 for production (SSL), 587 for dev
+  secure: isProduction, // true in production, false in development
   auth: {
     user: nodemailerEmail,
     pass: nodemailerPassword,
   },
-  logger: true,
-  debug: true,
+  logger: true, // logs SMTP conversation
+  debug: true,  // prints debugging info in console
 });
 
 export const sendMail = async (to, sub, mess) => {
@@ -23,3 +37,5 @@ export const sendMail = async (to, sub, mess) => {
     html: mess,
   });
 };
+
+

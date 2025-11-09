6d54e0b81c7081b944aa11c65b0abeba059130a2
Abhishek <mankumbareabhi@gmail.com>
2025-11-09 12:19:03 +0530

    fix : trying to fix redeploy

--- Backend/index.js
+++ Backend/index.js
@@ -69,7 +69,7 @@ connectDb();
  // if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "client");
   app.use(express.static(frontendPath));
-  app.get("/*splat", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 // }

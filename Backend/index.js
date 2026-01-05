9074f8dee4a9a7441e3e9e01595c4d3cda263fd4
Abhishek <mankumbareabhi@gmail.com>
2026-01-05 21:43:23 +0530

    downgrade express to fix deployment

--- Backend/index.js
+++ Backend/index.js
@@ -76,7 +76,7 @@ if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "client");
   app.use(express.static(frontendPath));
 
- app.get("/:path(*)", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 

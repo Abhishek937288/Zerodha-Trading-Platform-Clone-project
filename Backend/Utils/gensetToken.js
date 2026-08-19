de902cf3a4aac22db5b1524c9d61b4716f2d05db
abhishek <mankumbareabhi@gmail.com>
2026-08-19 22:53:53 +0530

    gensetToken.js updated

--- Backend/Utils/gensetToken.js
+++ Backend/Utils/gensetToken.js
@@ -5,11 +5,12 @@ const secretKey = env("JWT_SECRET");
 
 const genandsetToken = (id, res) => {
   const token = jwt.sign({ id }, secretKey, { expiresIn: "15d" });
-  const isProduction = env("STATUS") !== "dev";
+  const isProduction = process.env.NODE_ENV === "production";
   res.cookie("token", token, {
     httpOnly: true,
     sameSite: isProduction ? "None" : "Lax",
     secure: isProduction,
+    path: "/",
     maxAge: 15 * 24 * 60 * 60 * 1000,
   });
 };
